72d65aa4d2400f549447a9e3c479c67f9f2f8542
BrandotheCluff <brandon.cluff2@gmail.com>
2022-05-13 10:23:18 -0600

    resending my assess 1

--- password.js
+++ password.js
@@ -4,10 +4,13 @@ const firstName = 'Jenny!';
 
 let x = 'Full-Metal Alchemist'
 let empty = ''
-console.log(x + ' is ' + x.length + ' code units long')
 
-if (x.length >= 10) {
-    console.log(`Way to go, ${firstName}! Your password is long enough!`)
-}else if(x.length < 10){
-    console.log(`Sorry, ${firstName}, password is not long enough!`)
-} 
+reader.question('is' + x + '10 code units long?', function(x) {
+    if (x.length >= 10) {
+        console.log(`Way to go, ${firstName}! Your password is long enough!`)
+    }else if(x.length < 10){
+        console.log(`Sorry, ${firstName}, password is not long enough!`)
+    } 
+
+});
+
